ddfeebd633b1306e4f9b7ceb30c3c37e9ad6c4c1
Joseph Mathew <Joseph Mathew@DESKTOP-C2KD9IC>
2017-11-30 10:52:25 -0800

    first demo change

--- starbucksUI/src/main/java/com/starbucks/StarbucksUiApplication.java
+++ starbucksUI/src/main/java/com/starbucks/StarbucksUiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarbucksUiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("new change");
 		SpringApplication.run(StarbucksUiApplication.class, args);
 	}
 }
