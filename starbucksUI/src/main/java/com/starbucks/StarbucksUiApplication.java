9db70f058b4b3b6d146852be44581fe801f0a553
Joseph Mathew <Joseph Mathew@DESKTOP-C2KD9IC>
2017-11-30 10:56:33 -0800

    removed the syso

--- starbucksUI/src/main/java/com/starbucks/StarbucksUiApplication.java
+++ starbucksUI/src/main/java/com/starbucks/StarbucksUiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarbucksUiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("new change");
 		SpringApplication.run(StarbucksUiApplication.class, args);
 	}
 }
